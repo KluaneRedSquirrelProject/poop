11a2d738ec90545240619d964cabebf0b9329cb3
Matt Strimas-Mackey <mstrimas@gmail.com>
2018-03-06 17:25:11 -0500

    2010 files processed

--- 02_poop_external.r
+++ 02_poop_external.r
@@ -29,7 +29,8 @@ pfile_2006 <- read_xlsx("data/krsp_fecal-samples_2006.xlsx",
   filter(year == 2006) %>% 
   select(id, grid = Grid, tag_left = TagLft, tag_right = TagRt,
          date, comments = `Trap comments`,
-         poop_id = `Poop Vial ID`, poop_time = `Poop Time`)
+         poop_id = `Poop Vial ID`, poop_time = `Poop Time`) %>% 
+  mutate(poop_time = as.integer(poop_time))
 # match to trapping based on grid, date, tags, and comments
 matches <- trapping %>% 
   filter(!is.na(comments), comments != "", year(date) == 2006) %>% 
@@ -122,9 +123,9 @@ poop_2007 <- matches %>%
   select(squirrel_id, trapping_id, year, vial_number, poop_id, poop_time, 
          comments)
 
-# 2009-11: 
+# 2009 & 2011: 
 pfile_2009 <- read_xlsx("data/krsp_fecal-samples_2008-12.xlsx") %>% 
-  filter(Year %in% 2009:2011, !is.na(Poop)) %>% 
+  filter(Year %in% c(2009,2011), !is.na(Poop)) %>% 
   mutate(date = as.character(Date),
          id = row_number(),
          LocX = loc_to_numeric(LocX) %>% round(1) %>% format(nsmall = 1) %>% trimws()) %>% 
@@ -134,10 +135,11 @@ pfile_2009 <- read_xlsx("data/krsp_fecal-samples_2008-12.xlsx") %>%
          date, 
          poop_id = Poop, poop_time = PoopTime.24h,
          comments = Comments) %>% 
-  distinct()
+  distinct() %>% 
+  mutate(poop_time = as.integer(poop_time))
 # match to trapping, both tags
 matches <- trapping %>% 
-  filter(year(date) %in% 2009:2011) %>% 
+  filter(year(date) %in% c(2009,2011)) %>% 
   rename(locx_t = locx, locy_t = locy, comments_t = comments) %>% 
   left_join(pfile_2009, ., by = c("grid", "date", "obs",
                                   "tag_left", "tag_right"))
@@ -149,7 +151,7 @@ matches <- filter(matches, !is.na(trapping_id))
 # one tag
 # left
 no_match <- trapping %>% 
-  filter(year(date) %in% 2009:2011) %>% 
+  filter(year(date) %in% c(2009,2011)) %>% 
   select(-tag_right, -comments) %>% 
   rename(locx_t = locx, locy_t = locy) %>% 
   left_join(no_match, ., by = c("grid", "date", "obs", "tag_left"))
@@ -161,7 +163,7 @@ no_match <- no_match %>%
   select(one_of(names(pfile_2009)))
 # right
 no_match <- trapping %>% 
-  filter(year(date) %in% 2009:2011) %>% 
+  filter(year(date) %in% c(2009,2011)) %>% 
   select(-tag_left, -comments) %>% 
   rename(locx_t = locx, locy_t = locy) %>% 
   left_join(no_match, ., by = c("grid", "date", "obs", "tag_right"))
@@ -173,13 +175,13 @@ problem_2009 <- no_match %>%
   filter(is.na(trapping_id)) %>% 
   select(one_of(names(pfile_2009)))
 # multiple matches
-multiples <- matches %>% 
+multiples_2009 <- matches %>% 
   group_by(id) %>% 
   filter(n() > 1) %>% 
   ungroup()
-matches <- filter(matches, !id %in% multiples$id)
+matches <- filter(matches, !id %in% multiples_2009$id)
 # remove ambiguous cases using location
-matches <- multiples %>% 
+matches <- multiples_2009 %>% 
   mutate(loc_match = coalesce(locx == locx_t & locy == locy_t, FALSE)) %>% 
   group_by(id) %>% 
   filter(sum(loc_match) == 1) %>% 
@@ -187,9 +189,9 @@ matches <- multiples %>%
   filter(loc_match) %>% 
   select(-loc_match) %>% 
   bind_rows(matches, .)
-multiples <- filter(multiples, !id %in% matches$id)
+multiples_2009 <- filter(multiples_2009, !id %in% matches$id)
 # remove ambiguous cases using comments
-matches <- multiples %>% 
+matches <- multiples_2009 %>% 
   mutate(com_match = coalesce(comments == comments_t, FALSE)) %>% 
   group_by(id) %>% 
   filter(sum(com_match) == 1) %>% 
@@ -197,9 +199,9 @@ matches <- multiples %>%
   filter(com_match) %>% 
   select(-com_match) %>% 
   bind_rows(matches, .)
-multiples <- filter(multiples, !id %in% matches$id)
+multiples_2009 <- filter(multiples_2009, !id %in% matches$id)
 # remove ambiguous cases using weight
-matches <- multiples %>% 
+matches <- multiples_2009 %>% 
   mutate(wgt_match = coalesce(weight == wgt, FALSE)) %>% 
   group_by(id) %>% 
   filter(sum(wgt_match) == 1) %>% 
@@ -207,18 +209,17 @@ matches <- multiples %>%
   filter(wgt_match) %>% 
   select(-wgt_match) %>% 
   bind_rows(matches, .)
-multiples <- filter(multiples, !id %in% matches$id)
+multiples_2009 <- filter(multiples_2009, !id %in% matches$id)
 # remove released
-matches <- multiples %>% 
+matches <- multiples_2009 %>% 
   filter(!str_detect(str_to_lower(comments_t), "releas|escape|handle")) %>% 
   group_by(id) %>% 
   filter(n() == 1) %>% 
   ungroup() %>% 
   bind_rows(matches, .)
-multiples <- filter(multiples, !id %in% matches$id)
-
+multiples_2009 <- filter(multiples_2009, !id %in% matches$id)
 # chose one from multiples
-matches <- multiples %>% 
+matches <- multiples_2009 %>% 
   group_by(id) %>% 
   slice(1) %>% 
   ungroup() %>% 
@@ -229,15 +230,125 @@ poop_2009 <- matches %>%
   select(squirrel_id, trapping_id, year, vial_number, poop_id, poop_time, 
          comments)
 
+# 2010
+pfile_2010 <- list.files("data/", "2010", full.names = TRUE) %>% 
+  map_df(read_xls) %>% 
+  filter(!is.na(Poop)) %>% 
+  mutate(date = as.character(Date),
+         id = row_number(),
+         LocX = loc_to_numeric(LocX) %>% round(1) %>% format(nsmall = 1) %>% trimws()) %>% 
+  select(id, grid = Grid, obs = Obs,
+         tag_left = Taglft, tag_right = Tagrt,
+         locx = LocX, locy = LocY, weight = SqWt,
+         date, 
+         poop_id = Poop, poop_time = Ptime,
+         comments = Comments) %>% 
+  distinct() %>% 
+  mutate(poop_time = str_replace(poop_time, ":", ""),
+         poop_time =  as.double(poop_time))
+# match to trapping, both tags
+matches <- trapping %>% 
+  filter(year(date) == 2010) %>% 
+  rename(locx_t = locx, locy_t = locy, comments_t = comments) %>% 
+  left_join(pfile_2010, ., by = c("grid", "date", "obs",
+                                  "tag_left", "tag_right"))
+# split off records that didn't match to trapping
+no_match <- matches %>% 
+  filter(is.na(trapping_id)) %>% 
+  select(one_of(names(pfile_2010)))
+matches <- filter(matches, !is.na(trapping_id))
+# one tag
+# left
+no_match <- trapping %>% 
+  filter(year(date) == 2010) %>% 
+  select(-tag_right, -comments) %>% 
+  rename(locx_t = locx, locy_t = locy) %>% 
+  left_join(no_match, ., by = c("grid", "date", "obs", "tag_left"))
+matches <- no_match %>% 
+  filter(!is.na(trapping_id)) %>% 
+  bind_rows(matches, .)
+no_match <- no_match %>% 
+  filter(is.na(trapping_id)) %>% 
+  select(one_of(names(pfile_2010)))
+# right
+no_match <- trapping %>% 
+  filter(year(date) == 2010) %>% 
+  select(-tag_left, -comments) %>% 
+  rename(locx_t = locx, locy_t = locy) %>% 
+  left_join(no_match, ., by = c("grid", "date", "obs", "tag_right"))
+matches <- no_match %>% 
+  filter(!is.na(trapping_id)) %>% 
+  bind_rows(matches, .)
+# no matches at all
+problem_2010 <- no_match %>% 
+  filter(is.na(trapping_id)) %>% 
+  select(one_of(names(pfile_2010)))
+# multiple matches
+multiples_2010 <- matches %>% 
+  group_by(id) %>% 
+  filter(n() > 1) %>% 
+  ungroup()
+matches <- filter(matches, !id %in% multiples$id)
+# remove ambiguous cases using location
+matches <- multiples_2010 %>% 
+  mutate(loc_match = coalesce(locx == locx_t & locy == locy_t, FALSE)) %>% 
+  group_by(id) %>% 
+  filter(sum(loc_match) == 1) %>% 
+  ungroup() %>% 
+  filter(loc_match) %>% 
+  select(-loc_match) %>% 
+  bind_rows(matches, .)
+multiples_2010 <- filter(multiples_2010, !id %in% matches$id)
+# remove ambiguous cases using comments
+matches <- multiples_2010 %>% 
+  mutate(com_match = coalesce(comments == comments_t, FALSE)) %>% 
+  group_by(id) %>% 
+  filter(sum(com_match) == 1) %>% 
+  ungroup() %>% 
+  filter(com_match) %>% 
+  select(-com_match) %>% 
+  bind_rows(matches, .)
+multiples_2010 <- filter(multiples_2010, !id %in% matches$id)
+# remove ambiguous cases using weight
+matches <- multiples_2010 %>% 
+  mutate(wgt_match = coalesce(weight == wgt, FALSE)) %>% 
+  group_by(id) %>% 
+  filter(sum(wgt_match) == 1) %>% 
+  ungroup() %>% 
+  filter(wgt_match) %>% 
+  select(-wgt_match) %>% 
+  bind_rows(matches, .)
+multiples_2010 <- filter(multiples_2010, !id %in% matches$id)
+# remove released
+matches <- multiples_2010 %>% 
+  filter(!str_detect(str_to_lower(comments_t), "releas|escape|handle")) %>% 
+  group_by(id) %>% 
+  filter(n() == 1) %>% 
+  ungroup() %>% 
+  bind_rows(matches, .)
+multiples_2010 <- filter(multiples_2010, !id %in% matches$id)
+# chose one from multiples
+matches <- multiples_2010 %>% 
+  group_by(id) %>% 
+  slice(1) %>% 
+  ungroup() %>% 
+  bind_rows(matches, .)
+# prepare the poop table records
+poop_2010 <- matches %>% 
+  mutate(year = year(date), 
+         vial_number = 1) %>% 
+  select(squirrel_id, trapping_id, year, vial_number, poop_id, poop_time, 
+         comments)
+
 # combine problems
-problems <- bind_rows(problem_2006, problem_2007, problem_2009)
+problems <- bind_rows(problem_2006, problem_2007, problem_2009, problem_2010)
 write_csv(problems, "output/poop_no-matches.csv")
-multiples <- multiples %>% 
+multiples <- bind_rows(multiples_2009, multiples_2010) %>% 
   select(names(problems) %>% c("trapping_id", "squirrel_id") %>% one_of())
 write_csv(multiples, "output/poop_duplicates.csv")
 
 # combine poop
-poop <- bind_rows(poop_2006, poop_2007, poop_2009) %>% 
+poop <- bind_rows(poop_2006, poop_2007, poop_2009, poop_2010) %>% 
   filter(!poop_id %>% tolower() %>% str_detect("no"))
 # split out record with two ids in poop_id field
 poop <- poop %>% 
